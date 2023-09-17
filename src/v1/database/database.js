d65f3ccb57f3f27358159510689c5bf1e18b7072
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-09-17 17:49:14 -0600

    Pass required parameters to the createConnection method

--- src/v1/database/database.js
+++ src/v1/database/database.js
@@ -1,4 +1,7 @@
 const mysql = require('mysql');
 const dbconnection = mysql.createConnection({
-    
-})
+  host: 'localhost',
+  user: 'user',
+  password: 'p@ssw0rd145',
+  database: 'schooldir',
+});
