c54874c9b0878dd38c48d2f11b6d6be647bf206e
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-09-16 21:44:33 -0600

    Add listening method

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello from building a REST API in JavaScript with Express');
 });
+
+app.listen(port, () =>
+  console.log(`Running the API. We are listening on port ${port}!`)
+);
