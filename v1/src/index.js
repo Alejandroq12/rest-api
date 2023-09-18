7e221b200aed7c9d57d8a85b5a28df4c0c20a2c5
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-09-17 20:17:29 -0600

    Add data to be served

--- v1/src/index.js
+++ v1/src/index.js
@@ -5,3 +5,13 @@ const cors = require('cors');
 
 const app = express();
 const port = 3000;
+
+// Array of jokes to serve
+let jokes = [
+    { id: 1, joke: "Why couldn't the bicycle stand up by itself? Because it was two-tired." },
+    { id: 2, joke: "I'm reading a book on anti-gravity. It's impossible to put down." },
+    { id: 3, joke: "Did you hear about the guy who invented Lifesavers? He made a mint." },
+    { id: 4, joke: "I used to be a baker, but I couldn't make enough dough." },
+    { id: 5, joke: "What do you call a pile of cats? A meowntain." },
+    { id: 6, joke: "What do you get when you cross a snowman and a vampire? Frostbite." }
+  ];
