72fad0d0b976217b5df2c0a99e457df390b05441
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-09-17 19:48:12 -0600

    Get started with a better folder structure

--- v1/src/database/database.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql');
-const dbconnection = mysql.createConnection({
-  host: 'localhost',
-  user: 'user',
-  password: 'p@ssw0rd145',
-  database: 'schooldir',
-});
-dbconnection.connect();
--- v1/src/teacherarray.js
+++
@@ -1,8 +0,0 @@
-const teachers = [
-  'John Smith',
-  'Deshan Jackson',
-  'Susan Johson',
-  'Alexis simpson',
-];
-
-teachers.forEach((e) => console.log(e));
